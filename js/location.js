88e78b6541f73e99d664b2e05c9a986307857545
Carrie Chow <carriercat@gmail.com>
2012-12-01 18:17:36 -0800

    frontend and geolocation basics

---
+++ js/location.js
@@ -0,0 +1,18 @@
+function getLocation()
+{
+    if (navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else
+    {
+        $("#location").html("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position)
+{
+    $("#location").html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+}
+$(document).ready(function(){
+    getLocation();
+});
